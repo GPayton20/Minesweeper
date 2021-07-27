3484b15eb183a6bb5cbfc2e19f58d9985bb4c714
Geoffrey <geoffrpayton@gmail.com>
2021-07-27 18:45:02 -0400

    first commit

---
+++ cell.js
@@ -0,0 +1,109 @@
+import { game } from "./index.js";
+
+class Cell {
+  constructor(row, column) {
+    this.row = row;
+    this.column = column;
+    this.covered = true;
+    this.flagged = false;
+    this.mine = false;
+  }
+
+  isMine() {
+    game.revealMines();
+    game.showModal("LOSE");
+  }
+
+  uncover() {
+    this.covered = false;
+    game.remainingSafeCells -= 1;
+    game.showSafeCellsRemaining();
+  }
+
+  getGridElement() {
+    return document.querySelector(
+      `[data-row='${this.row}'][data-column='${this.column}']`
+    );
+  }
+
+  getNeighbours() {
+    let neighbours = [];
+    for (let i = this.row - 1; i <= this.row + 1; i++) {
+      for (let j = this.column - 1; j <= this.column + 1; j++) {
+        if (i >= 0 && i < 10 && j >= 0 && j < 10) {
+          neighbours.push(game.grid[i][j]);
+        }
+      }
+    }
+    return neighbours;
+  }
+
+  getNeighbouringMineCount(cells) {
+    this.neighbouringMines = cells.filter((cell) => cell.mine).length;
+  }
+
+  showNeighbouringMineCount(element = this.getGridElement()) {
+    element.textContent = this.neighbouringMines || "";
+  }
+
+  replaceButton() {
+    const element = this.getGridElement();
+    if (element.nodeName === "BUTTON") {
+      const div = document.createElement("div");
+      div.classList.add("grid-cell");
+      div.dataset.row = element.dataset.row;
+      div.dataset.column = element.dataset.column;
+      element.replaceWith(div);
+    }
+  }
+
+  handleClick(event) {
+    // The first button the user clicks will never be a bomb
+    if (!game.firstButtonClicked) {
+      if (this.mine) {
+        this.mine = false;
+        game.placeMines(1);
+      }
+      game.firstButtonClicked = true;
+    }
+
+    if (this.mine) {
+      this.isMine();
+    } else {
+      if (this.covered) {
+        this.uncover();
+        if (this.flagged) {
+          this.handleFlag(event);
+        }
+        this.replaceButton();
+        this.getNeighbouringMineCount(this.getNeighbours());
+        this.showNeighbouringMineCount();
+        // If cell has no neighbouring mines, uncover all neighbouring cells
+        // and repeat loop for each
+        if (this.neighbouringMines === 0) {
+          this.getNeighbours().forEach((neighbour) => neighbour.handleClick());
+        }
+      }
+    }
+    game.checkWin();
+  }
+
+  handleFlag(event) {
+    event.preventDefault();
+    const element = this.getGridElement();
+
+    if (this.flagged === false) {
+      this.flagged = true;
+      game.flaggedCells += 1;
+      element.textContent = "🏴‍☠️";
+      game.showFlaggedCells();
+    } else {
+      this.flagged = false;
+      game.flaggedCells -= 1;
+      element.textContent = "";
+      game.showFlaggedCells();
+    }
+  }
+}
+
+export default Cell;
---
+++ index.js
@@ -0,0 +1,111 @@
+import Cell from "./cell.js";
+
+const gridContainer = document.getElementById("grid");
+const safeCellsContainer = document.getElementById("safeCells");
+const flaggedCellsContainer = document.getElementById("flaggedCells");
+const overlay = document.getElementById("overlay");
+const modalText = document.getElementById("modalText");
+const playAgainButton = document.getElementById("playAgain");
+
+export const game = {};
+
+game.init = () => {
+  game.hideModal();
+  game.numberOfMines = 15;
+  game.firstButtonClicked = false;
+  game.grid = game.buildGrid(10, 10);
+  game.emptyGrid();
+  game.displayGrid(game.grid);
+  game.placeMines(game.numberOfMines);
+  game.remainingSafeCells = 100 - game.numberOfMines;
+  game.showSafeCellsRemaining();
+  game.flaggedCells = 0;
+  game.showFlaggedCells();
+};
+
+game.emptyGrid = () => {
+  gridContainer.replaceChildren();
+};
+
+game.buildGrid = (rows, columns) => {
+  const grid = [];
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < columns; j++) {
+      const cell = new Cell(i, j);
+      row.push(cell);
+    }
+    grid.push(row);
+  }
+  return grid;
+};
+
+game.displayGrid = (grid) => {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      const cell = game.grid[i][j];
+      const button = document.createElement("button");
+      button.classList.add("grid-cell");
+      button.dataset.row = i;
+      button.dataset.column = j;
+
+      button.addEventListener("click", cell.handleClick.bind(cell));
+      button.addEventListener("contextmenu", cell.handleFlag.bind(cell));
+
+      gridContainer.appendChild(button);
+    }
+  }
+};
+
+game.placeMines = (number) => {
+  let cells = game.grid.flat();
+  let i = 0;
+  while (i < number) {
+    const randomCell = cells[Math.floor(Math.random() * cells.length)];
+    const selectedCell = game.grid[randomCell.row][randomCell.column];
+    if (!selectedCell.mine && selectedCell.covered) {
+      selectedCell.mine = true;
+      i++;
+    }
+  }
+};
+
+game.showSafeCellsRemaining = () => {
+  safeCellsContainer.textContent = game.remainingSafeCells;
+};
+
+game.showFlaggedCells = () => {
+  flaggedCellsContainer.textContent = game.flaggedCells;
+};
+
+game.revealMines = () => {
+  const elements = Array.from(gridContainer.children);
+  elements.forEach((element) => {
+    if (game.grid[element.dataset.row][element.dataset.column].mine) {
+      element.textContent = "💣";
+    }
+  });
+};
+
+game.checkWin = () => {
+  if (game.remainingSafeCells === 0) {
+    game.showModal("WIN");
+  }
+};
+
+game.showModal = (condition) => {
+  overlay.classList.remove("hidden");
+  if (condition === "WIN") {
+    modalText.textContent = "Congratulations, you win!";
+  } else if (condition === "LOSE") {
+    modalText.textContent = "Oh no! You lose!";
+  }
+};
+
+game.hideModal = () => {
+  overlay.classList.add("hidden");
+};
+
+playAgainButton.addEventListener("click", game.init);
+
+game.init();
